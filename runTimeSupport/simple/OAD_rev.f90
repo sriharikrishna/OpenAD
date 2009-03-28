46c8f6aa620da71d37e706abe803b4c77bb97e35
m_utkej <m_utkej@anlextwls096-216>
2009-03-28 00:19:23 -0500

    renames + extras

--- runTimeSupport/simple/OAD_rev.f90
+++ runTimeSupport/simple/OAD_rev.f90
@@ -56,7 +56,8 @@ module OAD_rev
 
   private
   public :: modeType, our_rev_mode, OAD_revInit, &
-OAD_revStorePlain, OAD_revAdjoint, OAD_revRestoreTape
+OAD_revPlain, OAD_revTape, OAD_revAdjoint, & 
+OAD_revStorePlain, OAD_revRestoreTape
   
   type modeType
      logical :: arg_store=.FALSE.
@@ -74,16 +75,24 @@ OAD_revStorePlain, OAD_revAdjoint, OAD_revRestoreTape
      module procedure init
   end interface
   
-  interface OAD_revStorePlain
-     module procedure StorePlain
+  interface OAD_revPlain
+     module procedure plain
+  end interface
+  
+  interface OAD_revTape
+     module procedure tape
   end interface
   
   interface OAD_revAdjoint
-     module procedure Adjoint
+     module procedure adjoint
+  end interface
+  
+  interface OAD_revStorePlain
+     module procedure storePlain
   end interface
   
   interface OAD_revRestoreTape
-     module procedure RestoreTape
+     module procedure restoreTape
   end interface
   
 contains
@@ -98,8 +107,8 @@ contains
     our_rev_mode%adjoint=.FALSE.
   end subroutine 
 
-  subroutine StorePlain()
-    our_rev_mode%arg_store=.TRUE.
+  subroutine plain()
+    our_rev_mode%arg_store=.FALSE.
     our_rev_mode%arg_restore=.FALSE.
     our_rev_mode%res_store=.FALSE.
     our_rev_mode%res_restore=.FALSE.
@@ -108,7 +117,17 @@ contains
     our_rev_mode%adjoint=.FALSE.
   end subroutine 
 
-  subroutine Adjoint()
+  subroutine tape()
+    our_rev_mode%arg_store=.FALSE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.FALSE.
+    our_rev_mode%tape=.TRUE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
+
+  subroutine adjoint()
     our_rev_mode%arg_store=.FALSE.
     our_rev_mode%arg_restore=.FALSE.
     our_rev_mode%res_store=.FALSE.
@@ -118,7 +137,17 @@ contains
     our_rev_mode%adjoint=.TRUE.
   end subroutine 
 
-  subroutine RestoreTape()
+  subroutine storePlain()
+    our_rev_mode%arg_store=.TRUE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.TRUE.
+    our_rev_mode%tape=.FALSE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
+
+  subroutine restoreTape()
     our_rev_mode%arg_store=.FALSE.
     our_rev_mode%arg_restore=.TRUE.
     our_rev_mode%res_store=.FALSE.
