9455bd6611c5c17affb9ee3582e3b17a25d7af51
utke on macbook <utke on macbook@localhost>
2013-12-18 11:37:48 -0600

    sync with the version in use by mitgcm

--- runTimeSupport/cpToFile/OAD_tape.f90
+++ runTimeSupport/cpToFile/OAD_tape.f90
@@ -108,23 +108,23 @@ contains
   end subroutine init
 
   subroutine dump_tapestats()
-    write(*,'(A,I8,A,I8)',ADVANCE='NO') & 
-         ' TD:',oad_dt_ptr,' TI:',oad_it_ptr
+    write(*,'(3(A,I9))',ADVANCE='NO') & 
+         ' TD:',oad_dt_ptr,' TI:',oad_it_ptr, ' TS:',oad_st_ptr
   end subroutine dump_tapestats
 
   subroutine dt_grow
     integer status
-    print *, "DT+ ", oad_dt_sz
+    print *, "OAD: DT+ ", oad_dt_sz
     allocate(dtt(oad_dt_sz),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (1)failed with', status
+       print *,'OAD: allocation (1)failed with', status
        stop
     end if
     dtt=oad_dt
     deallocate(oad_dt)
     allocate(oad_dt(oad_dt_sz+increment),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (2)failed with', status
+       print *,'OAD: allocation (2)failed with', status
        stop
     end if
     oad_dt(1:oad_dt_sz) = dtt
@@ -134,17 +134,17 @@ contains
 
   subroutine it_grow
     integer status
-    print *, "IT+ ", oad_it_sz
+    print *, "OAD: IT+ ", oad_it_sz
     allocate(itt(oad_it_sz),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (1)failed with', status
+       print *,'OAD: allocation (1)failed with', status
        stop
     end if
     itt=oad_it
     deallocate(oad_it)
     allocate(oad_it(oad_it_sz+increment),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (2)failed with', status
+       print *,'OAD: allocation (2)failed with', status
        stop
     end if
     oad_it(1:oad_it_sz) = itt
@@ -154,17 +154,17 @@ contains
 
   subroutine lt_grow
     integer status
-    print *, "LT+ ", oad_lt_sz
+    print *, "OAD: LT+ ", oad_lt_sz
     allocate(ltt(oad_lt_sz),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (1)failed wlth', status
+       print *,'OAD: allocation (1)failed wlth', status
        stop
     end if
     ltt=oad_lt
     deallocate(oad_lt)
     allocate(oad_lt(oad_lt_sz+increment),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (2)failed wlth', status
+       print *,'OAD: allocation (2)failed wlth', status
        stop
     end if
     oad_lt(1:oad_lt_sz) = ltt
@@ -174,17 +174,17 @@ contains
 
   subroutine st_grow
     integer status
-    print *, "ST+ ", oad_st_sz
+    print *, "OAD: ST+ ", oad_st_sz
     allocate(stt(oad_st_sz),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (1)failed wsth', status
+       print *,'OAD: allocation (1)failed wsth', status
        stop
     end if
     stt=oad_st
     deallocate(oad_st)
     allocate(oad_st(oad_st_sz+increment),STAT=status)
     if (status .gt. 0 ) then
-       print *,'allocation (2)failed wsth', status
+       print *,'OAD: allocation (2)failed wsth', status
        stop
     end if
     oad_st(1:oad_st_sz) = stt
